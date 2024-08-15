91e6316fde39c128a4d016aa6ace6c61125de181
Shayan I Janjua <shayanjanjua.london@gmail.com>
2024-08-16 00:43:01 +0100

    Nearly done

--- app/page.js
+++ app/page.js
@@ -1,95 +1,46 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client';
+import { Backdrop, Box, Button, Stack, Typography, Paper } from '@mui/material';
+import { useState, useRef, useEffect } from 'react';
+import { useSession, signIn, signOut } from 'next-auth/react';
+import GoogleIcon from '@mui/icons-material/Google';
 
 export default function Home() {
+  const { data: session } = useSession();
+  useEffect(() => {
+    if (session?.user) {
+      setUsername(session.user.name);
+      setImage(session.user.image);
+    }
+  }, [session]);
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <Box >
+      <video src="/waitlistBg.mp4" autoPlay loop muted style={{ position: 'absolute', width: '100%', height: '100%', objectFit: 'cover',zIndex: -1,}}/>
+      <Box width="100vw"height="100vh"display="flex"justifyContent="center"alignItems="center"bgcolor=""position="relative">
+        {!session ? (
+          <Paper square={false} sx={{backgroundColor: 'rgba(0, 0, 0, 0.5)', padding: 4, borderRadius: '16px',  }}> 
+          <Stack direction={'column'} alignContent={'center'} alignItems={'center'}>
+          <Typography variant='h4' color={'white'}>Welcome to the Flip Efficiency waitlisting page!</Typography>
+          <br/>
+          <Typography variant='h6' color={'white'}>Please press the button below to sign up.</Typography>
+          <br/><br/><br/>
+          <Button
+            onClick={() => signIn('google')}
+            startIcon={<GoogleIcon />}
+            variant="contained"
+            sx={{ color: '#0B0C10', backgroundColor: 'white', '&:hover': { backgroundColor: '#f5e2cf' } }}
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+            Sign In With Google
+          </Button>
+          </Stack>
+          </Paper>
+        ) : (
+          <Stack direction={'column'} alignContent={'center'} alignItems={'center'}>
+            <Typography variant='h4' color={'black'}>Thanks for signing up!</Typography>
+            <Typography variant='h6' color={'black'}>You may now leave this page.</Typography>
+          </Stack>
+        )}
+      </Box>
+    </Box>
   );
 }
